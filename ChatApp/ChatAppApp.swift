a994df940bc6079fe9a6a7f8aa29d6ccca5a2981
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-12-19 12:15:38 +0530

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Arjun Shenoy on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatAppTests/ChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppTests.swift
+//  ChatAppTests
+//
+//  Created by Arjun Shenoy on 19/12/25.
+//
+
+import Testing
+@testable import ChatApp
+
+struct ChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
